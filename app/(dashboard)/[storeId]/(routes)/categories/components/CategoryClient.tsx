29f7fe49497272124e02ce7c453d08a33f8b9d82
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-07-02 12:03:49 +0200

    Finis categories - update prisma schema - add routes (API endpoints) - finish category front end

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/CategoryClient.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { CategoryColumn, columns } from "./Columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+const CategoryClient = ({ data }: CategoryClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categories(${data.length})`}
+          description="Manage categories for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Categories" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryIds" />
+    </>
+  );
+};
+
+export default CategoryClient;

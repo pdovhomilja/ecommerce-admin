bbc2530ad64a0e03a6000d7d0a25f04971561e0d
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-07-02 15:50:29 +0200

    Finish colors - update prisma scheme - complete routes (API endpoints) - complete front end functions (CRUD)

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/components/color-form.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { useState } from "react";
+import * as z from "zod";
+import { Color } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useParams, useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import AlertModal from "@/components/modals/alert-modal";
+
+interface ColorFormProps {
+  initialData: Color | null;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  value: z
+    .string()
+    .min(4)
+    .regex(/^#([0-9a-f]{3}){1,2}$/i, {
+      message: "String must be a valid color value",
+    }),
+});
+
+type ColorFormValues = z.infer<typeof formSchema>;
+
+const ColorForm = ({ initialData }: ColorFormProps) => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit color" : "New color";
+  const description = initialData ? "Edit your color" : "Create a new color";
+  const toastMessage = initialData ? "Color updated" : "Color created";
+  const action = initialData ? "Save changes" : "Create color";
+
+  const form = useForm<ColorFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      value: "",
+    },
+  });
+
+  //Action triggered when the form is submitted to update the store settings
+  const onSubmit = async (data: ColorFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/colors/${params.colorId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/colors`, data);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/colors`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+    console.log(data);
+  };
+
+  //Action triggered when the delete button is clicked to delete the store
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/colors/${params.colorId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/colors`);
+      toast.success("Color deleted");
+    } catch (error) {
+      toast.error("Make sure you have no products using this color first");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant={"destructive"}
+            size={"sm"}
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="w-4 h-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Color name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-x-4">
+                      <Input
+                        disabled={loading}
+                        placeholder="Value"
+                        {...field}
+                      />
+                      <div
+                        className="border p-4 rounded-full"
+                        style={{ backgroundColor: field.value }}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default ColorForm;
---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/ColorClient.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { ColorColumn, columns } from "./Columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizesClientProps {
+  data: ColorColumn[];
+}
+
+const ColorsClient = ({ data }: SizesClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors(${data.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorIds" />
+    </>
+  );
+};
+
+export default ColorsClient;
